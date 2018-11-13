b9011070156edb688cb7163e7c6026764a780270
Sander <sander@funkhaus.us>
2018-11-13 14:34:41 -0500

    Removed unnecessary local override

--- bin/fh-deploy.js
+++ bin/fh-deploy.js
@@ -3,23 +3,6 @@ const path = require('path')
 const fs = require('fs')
 const cosmiconfig = require('cosmiconfig')
 
-// Local version replace global one
-// taken from webpack: https://github.com/webpack/webpack/blob/master/bin/webpack.js#L10
-try {
-    const localDeploy = require.resolve(
-        path.join(
-            process.cwd(),
-            'node_modules',
-            'fh-deploy',
-            'bin',
-            'fh-deploy.js'
-        )
-    )
-    if (__filename !== localWebpack) {
-        return require(localWebpack)
-    }
-} catch (e) {}
-
 // get deploy command
 const deploy = require('../')
 
